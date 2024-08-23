420b869fcf9db752559a76875cde593ed2ffcce3
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2024-08-23 05:55:34 +0000

    Update: At least it's done.

---
+++ src/consolidate_csv_to_excel.py
@@ -0,0 +1,203 @@
+import datetime
+import logging
+import os
+import sys
+from logging import Logger
+from typing import List, Optional
+
+import pandas as pd
+import yaml
+from openpyxl import load_workbook
+
+
+class CustomLogger:
+    _LOG_FILE_PATH = os.path.join("log", "test.log")
+
+    def __init__(
+        self,
+        log_file_path: str = _LOG_FILE_PATH,
+        log_level: int = logging.INFO,
+    ) -> None:
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(log_level)
+
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setLevel(log_level)
+        formatter = logging.Formatter(
+            fmt="%(asctime)s - %(levelname)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        file_handler.setFormatter(formatter)
+        self._logger.addHandler(file_handler)
+
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(log_level)
+        console_handler.setFormatter(formatter)
+        self._logger.addHandler(console_handler)
+
+    @property
+    def get_logger(self) -> Logger:
+        return self._logger
+
+
+class CSVConsolidator:
+    _DATE_FORMAT = "%Y%m%d"
+    _LOG_FOLDER_PATH = os.path.join("test_dir_1", "test_dir_2")
+    _CONFIG_FILE_PATH = os.path.join("src", "config.yml")
+    _EXCEL_FOLDER_PATH = os.path.join("output", "excel")
+
+    def __init__(self) -> None:
+        self._logger = CustomLogger().get_logger
+
+    def _is_valid_date(self, input_date: str) -> bool:
+        try:
+            datetime.datetime.strptime(input_date, self._DATE_FORMAT)
+            return True
+        except ValueError:
+            return False
+
+    def _get_input_date_or_yesterday(self) -> str:
+        DATE = 1
+        if len(sys.argv) > 1:
+            input_date = sys.argv[DATE]
+
+            if self._is_valid_date(input_date):
+                return input_date
+            else:
+                error_message = (
+                    f"Invalid date specified: {input_date}."
+                    " Processing will be aborted."
+                )
+                self._logger.error(error_message)
+                raise ValueError(error_message)
+        else:
+            yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+            return yesterday.strftime(self._DATE_FORMAT)
+
+    def _get_targets_from_args_or_config(self) -> List[str]:
+        TARGET = 2
+        if len(sys.argv) > 2:
+            return sys.argv[TARGET].split(",")
+        else:
+            with open(self._CONFIG_FILE_PATH, "r") as file:
+                config = yaml.safe_load(file)
+                return config.get("targets", [])
+
+    def _determine_file_name_suffix(self, targets: List[str]) -> str:
+        if len(sys.argv) > 2:
+            return "_".join(targets)
+        else:
+            return "config"
+
+    def _create_excel_with_sentinel_sheet(self, excel_path: str) -> None:
+        with pd.ExcelWriter(excel_path, engine="openpyxl") as writer:
+            pd.DataFrame({"A": ["SENTINEL_SHEET"]}).to_excel(
+                writer, sheet_name="SENTINEL_SHEET", index=False, header=False
+            )
+        self._logger.info(
+            f"Initialized {excel_path} with a sentinel sheet"
+            " for further writing."
+        )
+
+    def _get_merged_csv_path(
+        self, host_folder_path: str, date: str
+    ) -> Optional[str]:
+        csv_name = f"test_{date}.csv"
+        csv_path = os.path.join(host_folder_path, csv_name)
+
+        if os.path.exists(csv_path):
+            self._logger.info(f"{csv_path} is found.")
+            return csv_path
+        else:
+            self._logger.info(f"{csv_path} is not found.")
+            return None
+
+    def _copy_csv_to_excel(
+        self, writer: pd.ExcelWriter, csv_path: str, host_name: str
+    ) -> None:
+        df = pd.read_csv(csv_path)
+        df.to_excel(writer, sheet_name=host_name, index=False)
+        self._logger.info(f"Added '{host_name}' sheet from file: {csv_path}.")
+
+    def _create_no_data_sheet_to_excel(
+        self, writer: pd.ExcelWriter, host_name: str
+    ) -> None:
+        df_for_not_found = pd.DataFrame({"A": ["No CSV file found."]})
+        df_for_not_found.to_excel(
+            writer, sheet_name=host_name, index=False, header=False
+        )
+
+        GRAY = "C0C0C0"
+        writer.sheets[host_name].sheet_properties.tabColor = GRAY
+
+        self._logger.info(
+            f"Wrote 'No CSV file found.' in cell A1 of '{host_name}' sheet."
+        )
+
+    def _add_sheet_for_target(
+        self, writer: pd.ExcelWriter, host_folder_path: str, date: str
+    ) -> None:
+        csv_file_path = self._get_merged_csv_path(host_folder_path, date)
+        host_name = os.path.basename(host_folder_path)
+
+        if csv_file_path:
+            self._copy_csv_to_excel(writer, csv_file_path, host_name)
+        else:
+            self._create_no_data_sheet_to_excel(writer, host_name)
+
+    def _folder_name_includes_target_name(
+        self, host_folder: str, targets: List[str]
+    ) -> bool:
+        return any(target in host_folder for target in targets)
+
+    def _search_and_append_csv_to_excel(
+        self,
+        log_directory: str,
+        date: str,
+        excel_path: str,
+        targets: List[str],
+    ) -> None:
+        with pd.ExcelWriter(excel_path, engine="openpyxl", mode="a") as writer:
+            for host_folder in os.listdir(log_directory):
+                if not self._folder_name_includes_target_name(
+                    host_folder, targets
+                ):
+                    continue
+
+                host_folder_path = os.path.join(log_directory, host_folder)
+                if os.path.isdir(host_folder_path):
+                    self._add_sheet_for_target(writer, host_folder_path, date)
+
+    def _remove_sentinel_sheet(self, excel_path: str) -> None:
+        workbook = load_workbook(excel_path)
+        if "SENTINEL_SHEET" in workbook.sheetnames:
+            del workbook["SENTINEL_SHEET"]
+            workbook.save(excel_path)
+            self._logger.info(f"Removed SENTINEL_SHEET from {excel_path}.")
+        else:
+            self._logger.warning(f"SENTINEL_SHEET not found in {excel_path}.")
+
+    def main(self) -> None:
+        self._logger.info("Process started.")
+
+        date = self._get_input_date_or_yesterday()
+        targets = self._get_targets_from_args_or_config()
+        self._logger.info(f"date: {date}")
+        self._logger.info(f"targets: {targets}")
+
+        file_name_suffix = self._determine_file_name_suffix(targets)
+        excel_name = f"{date}_{file_name_suffix}.xlsx"
+        excel_path = os.path.join(self._EXCEL_FOLDER_PATH, excel_name)
+
+        self._create_excel_with_sentinel_sheet(excel_path)
+        self._search_and_append_csv_to_excel(
+            self._LOG_FOLDER_PATH, date, excel_path, targets
+        )
+        self._remove_sentinel_sheet(excel_path)
+
+        self._logger.info("Process completed.")
+
+
+if __name__ == "__main__":
+    consolidator = CSVConsolidator()
+    consolidator.main()
---
+++ test.sh
@@ -0,0 +1,4 @@
+python src/consolidate_csv_to_excel.py
+python src/consolidate_csv_to_excel.py 20380101
+python src/consolidate_csv_to_excel.py 20380101 abc
+python src/consolidate_csv_to_excel.py 20380101 abc,def
