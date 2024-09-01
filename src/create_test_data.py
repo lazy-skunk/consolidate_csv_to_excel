7c97195e7c5d553bb1000f954d14ad5ea4b50f88
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2024-09-01 10:46:27 +0000

    Update: Minor modifications.

--- src/create_test_data.py
+++ src/create_test_data.py
@@ -4,39 +4,44 @@ from datetime import datetime, timedelta
 
 import pandas as pd
 
-_BASE_DATE = datetime.now()
-_data = []
 
-for i in range(10000):
-    date_a = _BASE_DATE - timedelta(seconds=i)
-    date_b = _BASE_DATE + timedelta(seconds=i)
+def create_test_csv() -> None:
+    BASE_DATE = datetime.now()
+    data = []
 
-    time_difference = int((date_b - date_a).total_seconds())
+    for i in range(10000):
+        date_a = BASE_DATE - timedelta(seconds=i)
+        date_b = BASE_DATE + timedelta(seconds=i)
+        processing_time = int((date_b - date_a).total_seconds())
+        random_value = random.choice([None, True])
 
-    random_value = random.choice([None, True])
-    json_list = []
-    for j in range(2):
+        json_list = []
         json_data = {
             "date_a": date_a.isoformat(),
             "date_b": date_b.isoformat(),
-            "time_difference": f"{time_difference}s",
+            "processing_time": f"{processing_time}s",
             "random_key": random_value,
         }
-        json_list.append(json_data)
+        for _ in range(2):
+            json_list.append(json_data)
 
-    stringified_json = json.dumps(json_list)
+        stringified_json = json.dumps(json_list)
 
-    _data.append(
-        [
-            date_a.strftime("%Y-%m-%d %H:%M:%S"),
-            date_b.strftime("%Y-%m-%d %H:%M:%S"),
-            f"{time_difference}s",
-            stringified_json,
-        ]
+        data.append(
+            [
+                date_a.strftime("%Y-%m-%d %H:%M:%S"),
+                date_b.strftime("%Y-%m-%d %H:%M:%S"),
+                f"{processing_time}s",
+                stringified_json,
+            ]
+        )
+
+    df = pd.DataFrame(
+        data, columns=["Date_A", "Date_B", "Processing_Time", "JSON"]
     )
 
-df = pd.DataFrame(
-    _data, columns=["Date A", "Date B", "Time Difference", "JSON"]
-)
+    df.to_csv("test_20380101.csv", index=False)
 
-df.to_csv("test_20380101.csv", index=False)
+
+if __name__ == "__main__":
+    create_test_csv()
