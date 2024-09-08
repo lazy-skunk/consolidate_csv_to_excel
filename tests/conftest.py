fcc69d03ea0ffbeb795ef51ee9ca6ddcd1d85352
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2024-09-08 13:28:56 +0000

    Update: Always patch _TARGET_FOLDERS_BASE_PATH.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,164 +3,69 @@ import logging
 import os
 from datetime import datetime, timedelta
 from pathlib import Path
-from unittest.mock import MagicMock
+from typing import Generator
+from unittest.mock import MagicMock, patch
 
 import pandas as pd
 import pytest
 from openpyxl import Workbook
 
-from src.consolidate_csv_to_excel import (
-    ConfigLoader,
-    CSVConsolidator,
-    DateHandler,
-    ExcelAnalyzer,
-    TargetHandler,
-)
 
-
-@pytest.fixture
-def mock_logger() -> MagicMock:
-    return MagicMock(spec=logging.Logger)
-
-
-@pytest.fixture
-def date_handler(mock_logger: MagicMock) -> DateHandler:
-    return DateHandler(mock_logger)
-
-
-@pytest.fixture
-def mock_config_loader() -> MagicMock:
-    return MagicMock(spec=ConfigLoader)
-
-
-@pytest.fixture
-def target_handler(
-    mock_config_loader: MagicMock, mock_logger: MagicMock
-) -> TargetHandler:
-    return TargetHandler(mock_config_loader, mock_logger)
-
-
-@pytest.fixture
-def csv_consolidator(mock_logger: MagicMock) -> CSVConsolidator:
-    return CSVConsolidator(mock_logger)
-
-
-@pytest.fixture
-def excel_analyzer(mock_logger: MagicMock) -> ExcelAnalyzer:
-    return ExcelAnalyzer(mock_logger)
-
-
-@pytest.fixture
-def tmp_path_for_excel(tmp_path: Path) -> str:
-    return os.path.join(tmp_path, "output.xlsx")
-
-
-@pytest.fixture
-def prepare_tmp_excel_with_sentinel(tmp_path_for_excel: str) -> None:
-    with pd.ExcelWriter(
-        tmp_path_for_excel, engine="openpyxl", mode="w"
-    ) as writer:
-        pd.DataFrame({"A": ["SENTINEL_SHEET"]}).to_excel(
-            writer, sheet_name="SENTINEL_SHEET", index=False, header=False
-        )
-
-
-@pytest.fixture
-def prepare_tmp_excel_with_sentinel_and_dummy(
-    tmp_path_for_excel: str, prepare_tmp_excel_with_sentinel: None
-) -> None:
-    with pd.ExcelWriter(
-        tmp_path_for_excel, engine="openpyxl", mode="a"
-    ) as writer:
-        pd.DataFrame({"A": ["VISIBLE_SHEET"]}).to_excel(
-            writer, sheet_name="VISIBLE_SHEET", index=False, header=False
-        )
-
-
-@pytest.fixture
-def prepare_tmp_csv(tmp_path: Path) -> None:
-    random_keys = [None, True, None, True]
-    for i in range(4):
-        data = []
-        valid_data = []
-        invalid_data = []
-
-        csv_file = f"{tmp_path}/target_{i}/test_19880209.csv"
-
-        csv_directory = os.path.dirname(csv_file)
-        os.makedirs(csv_directory, exist_ok=True)
-
-        date_a = datetime.now() - timedelta(seconds=i)
-        date_b = datetime.now() + timedelta(seconds=i)
-        processing_time = int((date_b - date_a).total_seconds())
-        json_list = [
-            {
-                "date_a": date_a.isoformat(),
-                "date_b": date_b.isoformat(),
-                "time_difference": f"{processing_time}s",
-                "random_key": random_keys[i],
-            }
-        ]
-
-        stringified_json = json.dumps(json_list)
-        valid_data.append(
-            [
-                date_a.strftime("%Y-%m-%d %H:%M:%S"),
-                date_b.strftime("%Y-%m-%d %H:%M:%S"),
-                f"{processing_time}s",
-                stringified_json,
-            ]
-        )
-
-        invalid_data.append(
-            [
-                date_a.strftime("%Y-%m-%d %H:%M:%S"),
-                date_b.strftime("%Y-%m-%d %H:%M:%S"),
-                "INVALID_PROCESSING_TIME",
-                "INVALID_JSON",
-            ]
-        )
-
-        data = valid_data + invalid_data
-
-        df = pd.DataFrame(
-            data, columns=["date_a", "date_b", "processing_time", "random_key"]
-        )
-        df.to_csv(csv_file, index=False)
-
-
-@pytest.fixture
-def prepare_tmp_excel(
-    prepare_tmp_csv: None,
+@pytest.fixture(autouse=True)
+def patch_target_folders_base_path(
     tmp_path: Path,
-    tmp_path_for_excel: str,
-) -> None:
-    with pd.ExcelWriter(
-        tmp_path_for_excel, engine="openpyxl", mode="w"
-    ) as writer:
-        for i in range(4):
-            csv_file = f"{tmp_path}/target_{i}/test_19880209.csv"
-            df = pd.read_csv(csv_file)
-            sheet_name = f"target_{i}"
-            df.to_excel(writer, sheet_name=sheet_name, index=False)
+) -> Generator[None, None, None]:
+    with patch(
+        "src.consolidate_csvs_to_excel._TARGET_FOLDERS_BASE_PATH", tmp_path
+    ):
+        yield
 
 
-@pytest.fixture
-def prepare_tmp_excel_for_reordering(tmp_path_for_excel: Path) -> None:
-    try:
-        workbook = Workbook()
+# @pytest.fixture
+# def prepare_tmp_excel_with_sentinel_and_dummy(
+#     tmp_path_for_excel: str, prepare_tmp_excel_with_sentinel: None
+# ) -> Generator[pd.ExcelWriter, None, None]:
+#     with pd.ExcelWriter(
+#         tmp_path_for_excel, engine="openpyxl", mode="a"
+#     ) as writer:
+#         pd.DataFrame({"A": ["VISIBLE_SHEET"]}).to_excel(
+#             writer, sheet_name="VISIBLE_SHEET", index=False, header=False
+#         )
+#         yield writer
 
-        default_sheet = workbook.active
-        workbook.remove(default_sheet)
 
-        workbook.create_sheet("Other_Sheet")
+# @pytest.fixture
+# def prepare_tmp_excel(
+#     prepare_tmp_csv: None,
+#     tmp_path: Path,
+#     tmp_path_for_excel: str,
+# ) -> None:
+#     with pd.ExcelWriter(
+#         tmp_path_for_excel, engine="openpyxl", mode="w"
+#     ) as writer:
+#         for i in range(4):
+#             csv_file = f"{tmp_path}/target_{i}/test_19880209.csv"
+#             df = pd.read_csv(csv_file)
+#             sheet_name = f"target_{i}"
+#             df.to_excel(writer, sheet_name=sheet_name, index=False)
 
-        gray_sheet = workbook.create_sheet("Gray_Sheet")
-        gray_sheet.sheet_properties.tabColor = "007F7F7F"
 
-        yellow_sheet = workbook.create_sheet("Yellow_Sheet")
-        yellow_sheet.sheet_properties.tabColor = "00FFFF7F"
+# @pytest.fixture
+# def prepare_tmp_excel_for_reordering(tmp_path_for_excel: Path) -> None:
+#     try:
+#         workbook = Workbook()
 
-        workbook.save(tmp_path_for_excel)
-    finally:
-        workbook.close()
+#         default_sheet = workbook.active
+#         workbook.remove(default_sheet)
+
+#         workbook.create_sheet("Other_Sheet")
+
+#         gray_sheet = workbook.create_sheet("Gray_Sheet")
+#         gray_sheet.sheet_properties.tabColor = "007F7F7F"
+
+#         yellow_sheet = workbook.create_sheet("Yellow_Sheet")
+#         yellow_sheet.sheet_properties.tabColor = "00FFFF7F"
+
+#         workbook.save(tmp_path_for_excel)
+#     finally:
+#         workbook.close()
