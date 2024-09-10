ccec98e6f773a131991ceb9902ce55a17ffb47fd
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2024-09-10 11:41:26 +0000

    Refactor: Minor modification.

--- src/create_test_data.py
+++ src/create_test_data.py
@@ -6,26 +6,23 @@ import pandas as pd
 
 
 def create_test_csv() -> None:
-    BASE_DATE = datetime.now()
+    BASE_DATE = datetime.fromisoformat("19880209")
     data = []
 
-    for i in range(10000):
+    for i in range(4):
         date_a = BASE_DATE - timedelta(seconds=i)
         date_b = BASE_DATE + timedelta(seconds=i)
         processing_time = int((date_b - date_a).total_seconds())
         random_value = random.choice([None, True])
 
-        json_list = []
         json_data = {
             "date_a": date_a.isoformat(),
             "date_b": date_b.isoformat(),
             "processing_time": f"{processing_time}s",
             "random_key": random_value,
         }
-        for _ in range(2):
-            json_list.append(json_data)
 
-        stringified_json = json.dumps(json_list)
+        stringified_json = json.dumps(json_data)
 
         data.append(
             [
@@ -40,7 +37,7 @@ def create_test_csv() -> None:
         data, columns=["Date_A", "Date_B", "Processing_Time", "JSON"]
     )
 
-    df.to_csv("test_20380101.csv", index=False)
+    df.to_csv("test_19880209.csv", index=False)
 
 
 if __name__ == "__main__":
